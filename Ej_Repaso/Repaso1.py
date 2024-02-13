a6e113f883178ee098234f99c4e7c79f13ea5981
Miguel Ángel Roldán de Haro <mayky23dh@gmail.com>
2024-02-13 18:30:47 +0100

    13/02/2024
    - Se resuelven las actividades básicas
    
    - Se comienza el Ej2 (sin terminar)

--- Ej_Repaso/Repaso1.py
+++ Ej_Repaso/Repaso1.py
@@ -1,31 +1,73 @@
 
-
 # Escribir un programa que imprima "Hola, mundo!" en la pantalla.
+print("Hola mundo")
 
 # Crear una cadena de caracteres que diga "Hola son las 6 de la tarde" y mostrarla en la pantalla de dos maneras diferentes.
+print("Hola son las " + str(6) + " de la tarde")
 
 # Utilizar la función len() para determinar la longitud de una lista o una cadena de caracteres.
+lista = [ 1, 2, 3, 4, 5]
+print(len(lista))
 
 # Solicitar al usuario que introduzca su nombre utilizando la función input() y luego saludarlo.
+nombre = input("Introduce tu nombre: ")
+print("Hola: ", nombre)
 
 # Utilizar un bucle for y la función range() para imprimir los números del 0 al 4 en la pantalla.
+for i in range (5):
+    print(i)
 
 # Determinar el tipo de una variable utilizando la función type().
+num = 10
+print(type(num))
 
 # Utilizar la función divmod() para obtener el cociente y el resto de la división de 7 entre 2.
+divmod(7,2)
 
 # Realizar operaciones matemáticas básicas como suma, resta, multiplicación, división, división entera y módulo.
+r_suma = 5 + 5
+r_resta = 5 - 3
+r_multiplicacion = 5 * 5
+r_div = 7 / 2
+r_modulo = 10 % 3
+
+print("resultados: ", r_suma, r_resta, r_multiplicacion, r_div, r_modulo)
 
 # Utilizar operadores de comparación para comparar valores y determinar si son iguales, mayores, menores, etc.
+a = 5
+b = 5
+
+print(a == b)
+print(a > b)
+print(a < b)
+print(a >= b)
+print(a <= b)
+print(a != b)
 
 # Utilizar la función abs() para obtener el valor absoluto de un número.
+x = -20
+print(abs(x))
 
 # Convertir un número en su representación hexadecimal utilizando la función hex().
+w = 50
+print(hex(w))
 
 # Elevar un número a una potencia utilizando la función pow().
+r = 25
+rb = 50
+print(pow(r, rb))
 
 # Redondear un número utilizando la función round() con y sin especificar el número de decimales.
+numerito = 5.2023423532
+print(round(numerito))
+print(round(numerito, 3))
 
 # Obtener el valor ASCII de un carácter utilizando la función ord().
+caracter_ascii = 'a'
+print(ord(caracter_ascii))
 
 # Importar la biblioteca math y utilizar la función sqrt() para calcular la raíz cuadrada de un número.
+import math
+
+numero2 = 50
+print(math.sqrt(numero2))
---
+++ Ej_Repaso/Repaso2.py
@@ -0,0 +1,26 @@
+"""
+Crear un programa que solicite al usuario ingresar un número entero positivo. 
+Luego, el programa deberá calcular la raíz cuadrada de dicho número y redondear
+el resultado a dos decimales. Después, el programa imprimirá en la pantalla el 
+resultado de la raíz cuadrada junto con su valor redondeado, utilizando el 
+formato 'La raíz cuadrada de [número] es [resultado_raiz] y 
+redondeado es [resultado_redondeado]'. Además, el programa verificará si 
+el número ingresado es mayor que 10 y si es así, imprimirá 'El número ingresado 
+es mayor que 10', de lo contrario imprimirá 'El número ingresado es igual o menor que 10'."
+
+Este ejercicio involucra solicitar entrada al usuario, realizar cálculos matemáticos,
+utilizar estructuras condicionales (if), formatear la salida para imprimir en pantalla,
+y trabajar con valores enteros, la función input(), operaciones matemáticas básicas, l
+a biblioteca math para calcular la raíz cuadrada y la función round() para redondear números.
+
+"""
+import math
+
+numUser = input("Inserte un número entero positivo: ")
+
+
+r_cuadrada = math.sqrt(numUser)
+
+num_redondeo = round(r_cuadrada, 2)
+
+print("La raíz cuadrada de", numUser, "es", num_redondeo)
